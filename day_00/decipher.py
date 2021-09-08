f26034170f7ba9477455fd0e0da95176b0f9790a
Galina <gmonitor@student.21-school.ru>
2021-09-08 12:56:19 +0300

    Upgrade

--- day_00/decipher.py
+++ day_00/decipher.py
@@ -1,8 +1,6 @@
 import sys
 
-message = sys.argv[1]
-message = message.lower()
-message = message.split(' ')
+message = sys.argv[1].lower().split(' ')
 result = []
 for i in range(len(message)):
 	result.append(message[i][0])
